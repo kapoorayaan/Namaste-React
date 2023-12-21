fe46630761e3a79d4b3f14568a667e6493cbae66
Ayaan <ayaankapoor013@gmail.com>
2023-12-21 13:48:04 +0530

    modified acron

--- src/Components/InstaMart.js
+++ src/Components/InstaMart.js
@@ -5,14 +5,18 @@ const Section = ({ title, description, isVisible, setisVisible }) => {
     <div className="border border-black m-2 p-2">
       <h2 className="font bold text-xl">{title}</h2>
       {isVisible ? (
-        <button
-          className="font bold underline cursor-pointer"
-          onClick={() => {
-            setisVisible(false);
-          }}
-        >
-          Hide
-        </button>
+        <>
+          {" "}
+          <button
+            className="font bold underline cursor-pointer"
+            onClick={() => {
+              setisVisible(false);
+            }}
+          >
+            Hide
+          </button>
+          <p>{description}</p>
+        </>
       ) : (
         <button
           className="font bold underline cursor-pointer"
@@ -23,17 +27,12 @@ const Section = ({ title, description, isVisible, setisVisible }) => {
           Show
         </button>
       )}
-      {isVisible && <p>{description}</p>}
     </div>
   );
 };
 
 const InstaMart = () => {
-  const [Config, setConfig] = useState({
-    showAbout: false,
-    showBuy: false,
-    ShowCareer: false,
-  });
+  const [visibleSection, setvisibleSection] = useState();
   return (
     <div>
       <h1 className="font bold text-3xl p-2 m-2 "> InstaMart</h1>
@@ -42,36 +41,24 @@ const InstaMart = () => {
         description={
           "Lorem ipsum dolor sit amet consectetur adipisicing elit. Reprehenderit, quos temporibus repudiandae minus quae non error quaerat voluptates quas, cLorem ipsum dolor sit amet consectetur adipisicing elit. Reprehenderit, quos temporibus repudiandae minus quae non error quaerat voluptates quas, commodiLorem ipsum dolor sit amet consectetur adipisicing elit. Reprehenderit, quos temporibus repudiandae minus quae non error quaerat voluptates quas, commodiommodi veniam, quasi magnam? Earum sint illo quae eligendi excepturi provident"
         }
-        isVisible={Config.showAbout}
-        setisVisible={() =>
-          setConfig({
-            showAbout: true,
-          })
-        }
+        isVisible={visibleSection == "about"}
+        setisVisible={() => setvisibleSection("about")}
       />
       <Section
         title={"Buy from Instamart"}
         description={
           "Lorem ipsum dolor sit ametLorem ipsum dolor sit amet consectetur adipisicing elit. Reprehenderit, quos temporibus repudiandae minus quae non error quaerat voluptates quas, commodiLorem ipsum dolor sit amet consectetur adipisicing elit. Reprehenderit, quos temporibus repudiandae minus quae non error quaerat voluptates quas, commodiLorem ipsum dolor sit amet consectetur adipisicing elit. Reprehenderit, quos temporibus repudiandae minus quae non error quaerat voluptates quas, commodi consectetur adipisicing elit. Reprehenderit, quos temporibus repudiandae minus quae non error quaerat voluptates quas, commodi veniam, quasi magnam? Earum sint illo quae eligendi excepturi provident"
         }
-        isVisible={Config.showBuy}
-        setisVisible={() =>
-          setConfig({
-            showBuy: true,
-          })
-        }
+        isVisible={visibleSection == "buy"}
+        setisVisible={() => setvisibleSection("buy")}
       />
       <Section
         title={"Career Instamart"}
         description={
           "Lorem ipsum dolor Lorem ipsum dolor sit amet consectetur adipisicing elit. Reprehenderit, quos temporibus repudiandae minus quae non error quaerat voluptates quas, commodiLorem ipsum dolor sit amet consectetur adipisicing elit. Reprehenderit, quos temporibus repudiandae minus quae non error quaerat voluptates quas, commodisit amet consectetur adipisicing elit. Reprehenderit, quos temporibus repudiandae minus quae non error quaerat voluptates quas, commodi veniam, quasi magnam? Earum sint illo quae eligendi excepturi provident"
         }
-        isVisible={Config.ShowCareer}
-        setisVisible={() =>
-          setConfig({
-            ShowCareer: true,
-          })
-        }
+        isVisible={visibleSection == "career"}
+        setisVisible={() => setvisibleSection("career")}
       />
     </div>
   );
